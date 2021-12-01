958f2d9915118dcd75f872a67e2446fc93ee9fd8
adam <62824117+adam061606@users.noreply.github.com>
2021-12-01 10:07:41 +0800

    Add files via upload

---
+++ k.py
@@ -0,0 +1,35 @@
+firstset = randomList(0) #SSS
+randomList.pop(0) SST, STX, STX, TXS, TXT, XSS, XTT
+
+
+counter=0
+LEFT=None
+RIGHT=None
+for restString in randomList:
+     if firstset[0:1]  == restString[1:2] 
+        LEFT=restString
+        randomList.pop(counter)
+     if firstset[1:2] ==restString[0:1]
+        randomList.pop(counter)
+        RIGHT=restString
+     if LEFT !=None and RIGHT !=None:
+         break;
+
+#LEFT=XSS
+#RIGHT=SST
+# STX, STX, TXS, TXT, XTT        
+
+counter2=0
+for restString2 in randomList: 
+    if LEFT[0:1] <XS>==restString2[1:2]<TX>
+        randomList.pop(counter2)
+        LEFT=restString2[1:2]
+        
+    if RIGHT[1:2] <TX> ==restString2[0:1]<ST>
+        randomList.pop(counter2)
+        RIGHT=restString2[1:2]
+    #RIGHT=STX
+    #STX, TXS, TXT, XTT        
+    
+        
+     
