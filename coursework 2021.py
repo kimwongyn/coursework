fd12aa91774d33ff13c3db42846f729e7d6fdbf2
adam <62824117+adam061606@users.noreply.github.com>
2022-01-06 14:19:43 +0800

    Add files via upload

---
+++ coursework 2021.py
@@ -0,0 +1,12 @@
+# import openpyxl and tkinter modules
+import tkinter as tk
+from tkinter import filedialog
+from openpyxl import *
+from tkinter import *
+
+# globally declare wb and sheet variable
+root = Tk()
+
+
+# start the GUI
+root.mainloop()
